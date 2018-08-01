23db660813a50d240c4c27f2f38826dd60c829cc
BELTOL3011 <bela.tollking@gmail.com>
2018-08-01 16:55:03 +0200

    Pfad noch mal gefixt

--- BATCH/AssemblierenUndStarten.bat
+++ BATCH/AssemblierenUndStarten.bat
@@ -9,13 +9,13 @@ nasm kernal2.asm -o boot.bin -f bin
 echo -------------------------------------------------
 echo Fertig mit compilieren!!!
 echo Lösche vorherige Datei
-cd C:\Users\%USERNAME%\Desktop\my Operating system\qemu
+cd C:\Users\%USERNAME%\Desktop\"my Operating system"\qemu
 del boot.bin
-cd C:\Users\Bela\Desktop\nasm
+cd C:\Users\Bela\Desktop\my Operating system
 echo Datei wird kopiert...
-copy boot.bin C:\Users\%USERNAME%\Desktop\my Operating system\qemu
+copy boot.bin C:\Users\%USERNAME%\Desktop\"my Operating system"\qemu\
 echo Fertig!!
-cd C:\Users\%USERNAME%\Desktop\my Operating system\qemu
+cd C:\Users\%USERNAME%\Desktop\"my Operating system"\qemu
 set /p a="Enter dücken wenn zufrieden:"
 echo qemu Startet!
 qemu-system-x86_64 boot.bin
